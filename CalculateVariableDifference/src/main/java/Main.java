8f1b0f0232eb178658c74b027bf7efdb79dd5e1f
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-08 23:26:22 +0300

    Create Main.java

---
+++ CalculateVariableDifference/src/main/java/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        int value1 = 195; //переменная 1
+        int value2 = 210; //переменная 2
+        System.out.println(value2-value1); 
+    }
+}
