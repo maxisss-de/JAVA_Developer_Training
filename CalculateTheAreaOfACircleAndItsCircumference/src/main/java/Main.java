301c171cd21447d9e094317cc7226836ddbd6d94
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-09 00:07:13 +0300

    Update Main.java

--- CalculateTheAreaOfACircleAndItsCircumference/src/main/java/Main.java
+++ CalculateTheAreaOfACircleAndItsCircumference/src/main/java/Main.java
@@ -1,1 +1,22 @@
-xsdfds
+import java.util.Scanner;
+
+class Main {
+    public static final double PI = 3.14;
+
+    public static double calculateCircleArea(int radius) {
+        return PI * radius * radius;
+    }
+
+    public static double calculateCircleLength(int radius) {
+        return 2 * PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите радиус окружности в сантиметрах");
+        int radius = scanner.nextInt();
+        System.out.println("Площадь круга: " + calculateCircleArea(radius) + " см.кв.");
+        System.out.println();
+        System.out.println("Длина окружности: " + calculateCircleLength(radius) + " см.");
+    }
+}
