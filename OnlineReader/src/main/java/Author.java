c17e67ecb475caf3d8d8aa9bbeb629bee32fef42
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-09 02:29:30 +0300

    Create Author.java

---
+++ OnlineReader/src/main/java/Author.java
@@ -0,0 +1,12 @@
+public class Author {
+  
+    public String name;
+    public String surname;
+    public int age;
+
+    public Author(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
