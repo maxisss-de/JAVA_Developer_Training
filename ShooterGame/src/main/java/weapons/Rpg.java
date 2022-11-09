25f66fca55cb253f2074a79dd38b88c8e347a88a
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-10 01:43:03 +0300

    Create Rpg.java

---
+++ ShooterGame/src/main/java/weapons/Rpg.java
@@ -0,0 +1,10 @@
+package weapons;
+//import Weapon;
+
+public class Rpg extends Weapon {
+
+    @Override
+    public void shot() {
+        System.out.println("Сейчас я тебя бобахну из РПГ");
+    }
+}
