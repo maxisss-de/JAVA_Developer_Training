25aa81675010db8563c252f888c831e2d5214848
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-10 01:40:51 +0300

    Create BigGun.java

---
+++ ShooterGame/src/main/java/weapons/BigGun.java
@@ -0,0 +1,10 @@
+package weapons;
+//import Weapon;
+
+public class BigGun extends Weapon {
+
+    @Override
+    public void shot() {
+        System.out.println("Этот большой пистолет, есть только у меня и у Майкла Джексона");
+    }
+}
