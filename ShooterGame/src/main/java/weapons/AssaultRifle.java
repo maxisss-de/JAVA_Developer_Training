4f7a998965efc7fb7bfb18c84954229f5f3070e4
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-10 01:40:15 +0300

    Create AssaultRifle.java

---
+++ ShooterGame/src/main/java/weapons/AssaultRifle.java
@@ -0,0 +1,10 @@
+package weapons;
+//import Weapon;
+
+public class AssaultRifle extends Weapon {
+
+    @Override
+    public void shot() {
+        System.out.println("Ага, а у меня есть штурмовая винтовка");
+    }
+}
