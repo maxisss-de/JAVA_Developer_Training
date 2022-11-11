a54684b9fe9667d376f370933092bc8b3e81c69e
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-12 02:54:52 +0300

    Create Book.java

---
+++ Library/src/main/java/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString() {
+        return author;
+    }
+}
