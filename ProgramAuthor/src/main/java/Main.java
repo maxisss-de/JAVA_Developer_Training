c5e004c4ecf938a63dcd48f45fab6f1aa6468780
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-11 19:31:17 +0300

    Update Main.java

--- ProgramAuthor/src/main/java/Main.java
+++ ProgramAuthor/src/main/java/Main.java
@@ -2,13 +2,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println ("Петров Николай");
-    }
-}
-
-//------------------------------------------------------
-// Вариант №2
-class Main {
-    public static void main(String[] args) {
         System.out.println ("John Smith");
     }
 }
