e6be82aa6cd808dadcce90f7280e654d3cb89984
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2023-01-30 17:49:23 +0300

    Update Main.java

--- Music/src/main/java/Main.java
+++ Music/src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {                       // Это класс для реализации
     
-    public static void main(String[] args) throws Exception {    // 14.11) "throws Exception" добавляется вместе с этой командой см. 14.9)
-        Instrument[] instruments = {      // 11.1) Массив инструментов ([Первый индекс][] -номер инструмента, а [][второй индекс] - номер секунды.)
-            new Guitar(),                 // 11.2) = см. 12.2)
+    public static void main(String[] args) throws Exception {    // 14.11) "throws Exception" добавляется вместе с этой командой см.14.9)
+        Instrument[] instruments = {      // 11.1) Массив инструментов 
+                    new Guitar(),         // 11.2) = см. 12.2)
                     new Piano(),          // 11.3) = см. 12.3)
                     new Flute(),          // 11.4) = см. 12.4)
                     new Guitar(),         // 11.5) = см. 12.5)
         };
 
-        boolean[][] shouldPlay = {                           // 12.1) Информация о том, когда, какой инструмент будет играть
+        boolean[][] shouldPlay = {                           // 12.1) Информация о том, когда, какой инструмент будет играть ([Первый индекс][] -номер инструмента, а [][второй индекс] - номер секунды.).
                 {false, true, true, true, true, false},      // 12.2) Как будет играть Guitar
                 {true, false, true, false, true, false},     // 12.3) Как будет играть Piano
                 {false, false, true, true, false, false},    // 12.4) Как будет играть Flute
