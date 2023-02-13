459d48b33d976e37b01b82a7d4de9ed42391e144
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2023-02-14 00:02:38 +0300

    Create Guitar.java

---
+++ Music-II/src/main/java/Guitar.java
@@ -0,0 +1,7 @@
+public class Guitar extends Instrument {  // 3) Создали класс Guitar (это класс сын) он наследуется(extends) от класса Instrument (это класс отец)
+
+    @Override  // Проверка метода
+    public char playSound() {  	          // 4) Переопределили метод playSound(), если его будет вызывать класс Guitar, то будет работать этот метод
+        return '$';    		                // 5) Возвращает  '$'
+    }
+}
