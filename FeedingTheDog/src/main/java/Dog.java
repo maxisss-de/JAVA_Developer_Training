83ae2fbda56a1c4a3090f736140ff4e3e738eaf8
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2023-11-23 23:11:51 +0300

    Create Dog.java

---
+++ FeedingTheDog/src/main/java/Dog.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Dog {
+    private String name;
+    private int appetite;
+
+    public Dog(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+  
+  // Модифицируем метод eat() в классе Dog, что делаем: см. ниже, 1 и 2
+    public void setEat(Plate p) { 
+        p.setDecreaseFood(appetite);
+    }
+}
+
+// Метод eat(): 1. Когда мы просим собаку поесть, необходимо указать на тарелку из которой он должен кушать, для этого в качестве аргумента прокидываем в метод eat(Plate plate) ссылку на объект типа Plate.
+// Метод eat(): 2. Во время выполнения метода eat() мы считаем что в тарелке должно уменьшиться количество еды, в зависимости от аппетита собаки (так как он эту еду съест) ->  
+// -> Поэтому обращаемся к тарелке и вызываем у нее метод decreaseFood(передавая сюда аппетит собаки).
