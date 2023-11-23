22cc8cba150c479afdaa06ce65000acd749a3f4a
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2023-11-23 23:03:46 +0300

    Update Main.java

--- FeedingTheDog/src/main/java/Main.java
+++ FeedingTheDog/src/main/java/Main.java
@@ -4,8 +4,8 @@ class Main {
     public static void main(String[] args) {
         Dog dog = new Dog("Heimerich", 6);
         Plate plate = new Plate(300);
-        plate.getInfo(); // Было еды в тарелки
+        plate.getInfo();   // Было еды в тарелки
         dog.setEat(plate); // Cъела собака
-        plate.getInfo(); // Сейчас еды в тарелки
+        plate.getInfo();   // Сейчас еды в тарелки
     }
 }
