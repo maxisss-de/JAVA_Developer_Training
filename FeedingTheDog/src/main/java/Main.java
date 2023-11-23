d74dcf9549fca5b2620de4650af479bca19e5006
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2023-11-23 23:03:00 +0300

    Create Main.java

---
+++ FeedingTheDog/src/main/java/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Heimerich", 6);
+        Plate plate = new Plate(300);
+        plate.getInfo(); // Было еды в тарелки
+        dog.setEat(plate); // Cъела собака
+        plate.getInfo(); // Сейчас еды в тарелки
+    }
+}
